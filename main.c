3b015742032bb5aef13127214060c70d38a9e96b
Dexter M Haslem <git@dmh.fm>
2022-11-20 01:06:07 -0700

    cleanup, fix delay on first disp

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 #include "si7021.h"
 
 /* tmr0 goes off every second, update display this often*/
-#define TEMP_UPDATE_RATE_TICKS (5)
+#define TEMP_UPDATE_RATE_TICKS (45)
 
 static volatile uint8_t ticks = 0;
 static volatile bool parse_next = false;
 static volatile bool dirty = false;
 static uint8_t temp_scalar;
-static uint8_t digit_vals[3];
-static uint8_t disps[4];
-static uint8_t shiftcnt = 0;
+static uint8_t digit_vals[3]; /* broken down digits for display */
+static uint8_t disps[4];      /* actual display dec->7seg digit values */
+static uint8_t shiftcnt = 0;  /* number of digits to show (3 or 4 once temp read) */
 
-static void on_tmr0(void) {
+static void isr_on_tmr0(void) {
     ticks++;
     dirty = true;
 }
@@ -26,12 +26,9 @@ static void req_parse_temp(void) {
     digit_vals[1] = (uint8_t)temp_scalar / 10 % 10;
     digit_vals[2] = (uint8_t)temp_scalar % 10;
     
-    //printf("F = %f\r\n", temp_f);
-    
-    
     /*display enable bits go from lsb msb so put right most display first*/
     disps[0] = seg_nums[remain];
-    disps[1] = seg_nums[digit_vals[2]] | seg_dp;
+    disps[1] = seg_nums[digit_vals[2]] | seg_dp; 
     disps[2] = seg_nums[digit_vals[1]];
     disps[3] = seg_nums[digit_vals[0]];
     
@@ -45,7 +42,7 @@ static void req_parse_temp(void) {
 }
 
 static uint8_t digit_cathode_shift = 0;
-static void on_display_timer(void) {
+static void isr_on_display_timer(void) {
     //IO_RA0_Toggle();
 #if 1
     /* mux the pins in a very fast timer so they all appear visible
@@ -54,10 +51,6 @@ static void on_display_timer(void) {
     if (shiftcnt) {
         LATD = (uint8_t)~(((uint8_t)1) << digit_cathode_shift);
         LATC = disps[digit_cathode_shift];
-//        if (++digit_cathode_shift > 4) {
-//            digit_cathode_shift = 0;
-//        }
-        
         if (++digit_cathode_shift >= shiftcnt) {
             digit_cathode_shift = 0;
         }
@@ -68,48 +61,43 @@ static void on_display_timer(void) {
 void main(void) {
     SYSTEM_Initialize();
     
-    /* common cathode on port d, turn all digits off */
+    /* common cathode on port d, turn all digits off, clear portc drives */
     LATD = 0xff;
     LATC = 0x00;
            
-    TMR0_SetInterruptHandler(on_tmr0);   
-    TMR1_SetInterruptHandler(on_display_timer);
-    //TMR3_StartTimer();
-    //INTERRUPT_GlobalInterruptHighEnable();
-    //INTERRUPT_GlobalInterruptLowEnable();
-    /* enable interrupts before trying i2c */
+    TMR0_SetInterruptHandler(isr_on_tmr0);   
+    TMR1_SetInterruptHandler(isr_on_display_timer);
+    
+    /* enable interrupts before trying i2c as its int driven */
     INTERRUPT_GlobalInterruptEnable();
- 
 
-    __delay_ms(1);
+    __delay_ms(2);
     printf("\r\r\rresetting sensor..\r\n");
     
     si7021_soft_reset();
     
     printf("done, starting loop\r\n");   
    
+    /* ask right away so we dont start blank. use the bus holding measure for
+     * this reason, so we dont try to read it too early after and get NAK'd */
+    si7021_meas_rh_hold();
+    parse_next = true;
+    dirty = true;
+    
     while (1) {
-#if 1
-        /* every 1 second tick run logic */
         if (dirty) {
             if (parse_next) {
-                //printf("updating display\r\n");
                 req_parse_temp();
-                //printf("done2\r\n");
                 parse_next = false;
             }
             
             /* always refresh reading */
             si7021_meas_rh_nohold();
             dirty = false;
-        }
-        
-        if (ticks >= TEMP_UPDATE_RATE_TICKS) {
-            //printf("doing update\r\n");
+        } else if (ticks >= TEMP_UPDATE_RATE_TICKS) {
             parse_next = true;
             ticks = 0;
             continue;
         }
-#endif
     }
 }
